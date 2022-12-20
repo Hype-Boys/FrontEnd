4eab63273d1ab5af9044b1e806127633e9c18d39
TAERANG <s22051@gsm.hs.kr>
2022-12-20 19:39:34 +0900

    1

---
+++ public/main.js
@@ -0,0 +1,10 @@
+const HomeLogo = document.querySelector(".Home_logo");
+
+
+function Movemain(){
+    window.location = "../public/html/main.html";   
+}
+
+HomeLogo.addEventListener("click", Movemain);
+
+
