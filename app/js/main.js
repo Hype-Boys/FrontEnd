5f73d94e4e8f4317b188296eb8ecd0599265a07b
TAERANG <s22051@gsm.hs.kr>
2022-12-21 09:30:32 +0900

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 const HomeLogo = document.querySelector(".Home_logo");
+const joinbutton = document.querySelector(".join_form");
 
 function join() {
   window.location = "../../public/html/join.html";
 }
 
-const joinbutton = document.querySelector(".Login_form");
+
 
 joinbutton.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log("Gd");
   window.location = "../../public/html/join.html";
 });
