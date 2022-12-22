ee399877b0bf4ab543788c0d33c1f35645ad9770
TAERANG <s22051@gsm.hs.kr>
2022-12-22 13:04:24 +0900

    mbti update the page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 const joinbutton = document.querySelector(".join_form");
 const HomeLogo = document.querySelector(".Home_logo");
+const loginbutton = document.querySelector(".login_form");
+
+loginbutton.addEventListener("submit", (event) => {
+  event.preventDefault();
+  window.location = "../../public/html/mbti.html";
+  
+});
 
 
 HomeLogo.addEventListener("click", (event) => {
--- app/js/mbti.js
+++ app/js/mbti.js
@@ -1,8 +1,10 @@
 const inputmbti = document.querySelector(".input_mbti");
 
+
 const HomeLogo = document.querySelector(".Home_logo");
 
 HomeLogo.addEventListener("click", (event) => {
   event.preventDefault();
   window.location = "../../public/html/main.html";
 });
+
