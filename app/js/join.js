d05f3f5fe7cefcf974f970c453b344a35174dd15
googoo81 <shw9052@naver.com>
2022-12-28 16:56:23 +0900

    npm init

--- app/js/join.js
+++ app/js/join.js
@@ -2,13 +2,13 @@ const newlogin = document.querySelector(".login_form");
 
 newlogin.addEventListener("submit", (event) => {
   event.preventDefault();
-  window.location = "../../public/html/main.html";
+  window.location = "../../public/html/index.html";
 });
 
 const HomeLogo = document.querySelector(".Home_logo");
 
 HomeLogo.addEventListener("click", () => {
-  window.location = "../../public/html/main.html";
+  window.location = "../../public/html/index.html";
 });
 
 const PW_login = document.querySelector(".PW_login");
